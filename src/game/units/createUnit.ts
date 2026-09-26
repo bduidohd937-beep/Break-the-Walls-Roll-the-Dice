f23572b43b5d84163d24b310f2d2a9c54090e319
bduidohd937-beep <bduidohd937@gmail.com>
2026-09-26 13:55:45 +0900

    refactor: extract unit creation

---
+++ src/game/units/createUnit.ts
@@ -0,0 +1,17 @@
+import type { Team, Unit, UnitDef } from "../types";
+
+export function makeUnit(def: UnitDef, team: Team, x: number, uid: number): Unit {
+  return {
+    ...def,
+    uid,
+    team,
+    x,
+    currentHp: def.hp,
+    attackTimer: Math.random() * 0.5,
+    cooldownTimer: 0,
+    hitFlash: 0,
+    attackFlash: 0,
+    knockbackCount: 0,
+    alive: true,
+  };
+}
